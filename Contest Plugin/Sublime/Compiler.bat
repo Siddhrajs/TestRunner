5f22a566207e1413778e027dd6b4cba9b1432b5f
Siddhrajs <siddhrajsisodiya7@gmail.com>
2023-02-20 23:22:42 +0530

    created new branch and readme for github pages

--- Contest Plugin/Sublime/Compiler.bat
+++
@@ -1,7 +0,0 @@
-cls
-@ECHO OFF
-cd /d D:\Workspace\Coding
-break>JavaOutput.txt
-echo Program is Running
-java Main<CustomInput.txt>JavaOutput.txt
-echo Program Completed
--- Contest Plugin/Sublime/realCompiler.bat
+++
@@ -1,6 +0,0 @@
-cls
-@ECHO OFF
-cd /d D:\Workspace\Coding
-break>JavaOutput.txt
-java Main
-pause
